9a7e91030ba8b353c8aef182b4848cdb3ea23a92
fikriwado <mfikri.khoirurrizal@gmail.com>
2025-04-16 10:13:26 +0700

    feat: auto generate route module

--- src/stubs/module/routes.js
+++ src/stubs/module/routes.js
@@ -1,1 +1,8 @@
-// testing
+/*
+    Example Routes
+*/
+const __Module__ = () => import("./pages/__Module__.vue");
+
+const routes = [{ path: "/__module-kebab__", component: __Module__ }];
+
+export default routes;
