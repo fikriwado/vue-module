275e6ad7f81d3fcab379926ae046707dc97ffffb
fikriwado <mfikri.khoirurrizal@gmail.com>
2025-04-17 08:51:37 +0700

    feat: test menubar

--- src/main.js
+++ src/main.js
@@ -4,12 +4,19 @@ import router from "./common/routes/router";
 import i18n from "./common/i18n/i18n";
 import PrimeVue from "primevue/config";
 import Aura from "@primeuix/themes/aura";
+import Lara from "@primeuix/themes/lara";
+import "./common/styles/styles.css";
 
 const app = createApp(App);
 app.use(router).use(i18n);
 app.use(PrimeVue, {
   theme: {
     preset: Aura,
+    options: {
+      prefix: "fw",
+      darkModeSelector: false,
+      cssLayer: false,
+    },
   },
 });
 app.mount("#app");
